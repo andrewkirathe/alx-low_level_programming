ff9300ab09b756942c88f84e6cd3c8b7db79dc2d
andrewkirathe <andrewkirathe@gmail.com>
2023-09-11 06:21:05 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-*main - Entrypoint
+* main - Entrypoint
 *
-*Return: Always 0(success)
+* Return: Always 0(success)
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
 }
